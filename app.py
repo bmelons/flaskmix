550e6234f12b8cab04f76cdb412b3798f024a14e
bmelons <brizfauxfox@gmail.com>
2026-02-09 23:45:36 -0600

    janitorial work, big changes coming regarding many systems. check diff for config.json to catch a glimpse

--- app.py
+++ app.py
@@ -452,7 +452,7 @@ def setup_app():
         with open('characters.json', 'w') as outfile:
             json.dump({"characters":[]}, outfile)
     # check for directories
-    checks = ['./static','./static/comic-pages','./static/chapter-icons','./static/side-pages','./static/characters', './side-content-data', ]
+    checks = ['./static','./static/comic-pages','./static/chapter-icons','./static/side-pages','./static/characters', './side-content-data','page-data' ]
     for directory in checks:
         if fcheck(directory) == False:
             os.mkdir(directory)
